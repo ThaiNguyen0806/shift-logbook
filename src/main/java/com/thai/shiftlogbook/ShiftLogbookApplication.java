4d8f8c03e33c769d0deb2920a697441d58aa797d
ThaiNguyen0806 <nguyenhoangthai080607@gmail.com>
2026-07-30 15:00:29 +0700

    initial spring boot project

---
+++ src/main/java/com/thai/shiftlogbook/ShiftLogbookApplication.java
@@ -0,0 +1,13 @@
+package com.thai.shiftlogbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftLogbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiftLogbookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thai/shiftlogbook/ShiftLogbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thai.shiftlogbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftLogbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

a24c0375a7056836ca99551ce95f62dad1a7d603
ThaiNguyen0806 <nguyenhoangthai080607@gmail.com>
2026-07-31 16:45:17 +0700

    Complete JWT auth with springdoc, login/register endpoints, exception handling

---
+++ src/main/java/com/thai/shiftlogbook/dto/LoginResponse.java
@@ -0,0 +1,14 @@
+package com.thai.shiftlogbook.dto;
+
+public class LoginResponse {
+
+    private final String token;
+
+    public LoginResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
